bbc41a5c73bd9caffcba6c309f4512c2cd965f15
Sebastián Silva <sebastian@booxlab.com>
2019-08-22 14:22:40 -0500

    Comentarios en espanio

--- client.py
+++ client.py
@@ -1,44 +1,59 @@
-
 #!/usr/bin/env python3
-"""Script for Tkinter GUI chat client."""
+# Universidad Nacional Autónoma de México
+# Facultad de Ingeniería
+# Sistemas distribuidos
+# Código de cliente para proyecto de sockets
+
+# Integrantes: 
+#    * Silva García, Carlos Sebastián
+#    * 
+#    *  
+#    * 
+
+# Importamos las bibliotecas socket y threading, que ya vienen con la 
+# instalación de python.
+# Además, utilizamos tkinter, que nos permite desarrollar una GUI.
+
+# Importante correr el programa utilizando python3.
+
 from socket import AF_INET, socket, SOCK_STREAM
 from threading import Thread
 import tkinter
+UTF_8 = "utf8"
 
 
 def receive():
-    """Handles receiving of messages."""
+    # Espera recibir mensajes todo el tiempo.
     while True:
         try:
-            msg = client_socket.recv(BUFSIZ).decode("utf8")
+            msg = client_socket.recv(BUFSIZ).decode(UTF_8)
             msg_list.insert(tkinter.END, msg)
-        except OSError:  # Possibly client has left the chat.
+        except OSError:  # Probablemente el cliente dejó el chat.
             break
 
 
-def send(event=None):  # event is passed by binders.
-    """Handles sending of messages."""
+def send(event=None):  # Envía el mensaje
     msg = my_msg.get()
-    my_msg.set("")  # Clears input field.
-    client_socket.send(bytes(msg, "utf8"))
-    if msg == "{quit}":
+    my_msg.set("")  # Borra el campo de mensaje, porque ya se ha enviado
+    client_socket.send(bytes(msg, UTF_8))
+    if msg == "salir":
         client_socket.close()
         top.quit()
 
 
 def on_closing(event=None):
-    """This function is to be called when the window is closed."""
-    my_msg.set("{quit}")
+    # Esta función se llama cuando se va a cerrar la ventana.
+    my_msg.set("salir")
     send()
 
 top = tkinter.Tk()
-top.title("Chatter")
+top.title("Proyecto de Sockets")
 
 messages_frame = tkinter.Frame(top)
-my_msg = tkinter.StringVar()  # For the messages to be sent.
-my_msg.set("Type your messages here.")
-scrollbar = tkinter.Scrollbar(messages_frame)  # To navigate through past messages.
-# Following will contain the messages.
+my_msg = tkinter.StringVar()  # Campo para escribir.
+my_msg.set("Escribe aquí...")
+scrollbar = tkinter.Scrollbar(messages_frame)  # Agrega una barra de navegación para mensajes anteriores
+# Configuraciones para la barra de mensajes
 msg_list = tkinter.Listbox(messages_frame, height=15, width=50, yscrollcommand=scrollbar.set)
 scrollbar.pack(side=tkinter.RIGHT, fill=tkinter.Y)
 msg_list.pack(side=tkinter.LEFT, fill=tkinter.BOTH)
@@ -48,16 +63,20 @@ messages_frame.pack()
 entry_field = tkinter.Entry(top, textvariable=my_msg)
 entry_field.bind("<Return>", send)
 entry_field.pack()
-send_button = tkinter.Button(top, text="Send", command=send)
+send_button = tkinter.Button(top, text="Enviar", command=send)
 send_button.pack()
 
 top.protocol("WM_DELETE_WINDOW", on_closing)
 
-#----Now comes the sockets part----
+# --- Configuración de los sockets
+# Cambiamos los sockets a los mismos que utiliza el servidor.
+
 HOST = input('Enter host: ')
 PORT = input('Enter port: ')
+if not HOST: 
+    HOST = '127.0.0.1'
 if not PORT:
-    PORT = 33000
+    PORT = 8081
 else:
     PORT = int(PORT)
 
@@ -69,4 +88,4 @@ client_socket.connect(ADDR)
 
 receive_thread = Thread(target=receive)
 receive_thread.start()
-tkinter.mainloop()  # Starts GUI execution.
+tkinter.mainloop()  # Inicia la ventana
--- server.py
+++ server.py
@@ -1,63 +1,84 @@
 
 #!/usr/bin/env python3
-"""Server for multithreaded (asynchronous) chat application."""
+# Universidad Nacional Autónoma de México
+# Facultad de Ingeniería
+# Sistemas distribuidos
+# Código de servidor para proyecto de sockets
+
+# Integrantes: 
+#    * Silva García, Carlos Sebastián
+#    * 
+#    *  
+#    * 
+
+# Importamos las bibliotecas socket y threading, que ya vienen con la 
+# instalación de python.
+# Importante correr el programa utilizando python3.
+
+
 from socket import AF_INET, socket, SOCK_STREAM
 from threading import Thread
-
-
+UTF_8 = "utf8"
 def accept_incoming_connections():
-    """Sets up handling for incoming clients."""
+   # Maneja a los nuevos clientes, y los guarda en memoria.
     while True:
         client, client_address = SERVER.accept()
-        print("%s:%s has connected." % client_address)
-        client.send(bytes("Greetings from the cave! Now type your name and press enter!", "utf8"))
+        print("%s:%s se ha conectado." % client_address)
+        client.send(bytes("Saludos desde la H. Facultad de Ingeniería. Ingresa tu nombre: ", UTF_8))
         addresses[client] = client_address
         Thread(target=handle_client, args=(client,)).start()
 
 
-def handle_client(client):  # Takes client socket as argument.
-    """Handles a single client connection."""
-
-    name = client.recv(BUFSIZ).decode("utf8")
-    welcome = 'Welcome %s! If you ever want to quit, type {quit} to exit.' % name
-    client.send(bytes(welcome, "utf8"))
-    msg = "%s has joined the chat!" % name
-    broadcast(bytes(msg, "utf8"))
+def handle_client(client):  # Toma el socket client como argumento
+    # Maneja a cada cliente individualmente
+    name = client.recv(BUFSIZ).decode(UTF_8)
+    welcome = '¡Bienvenido,  %s! Si quieres salir, escribes "salir" y presiona enviar. ' % name
+    client.send(bytes(welcome, UTF_8))
+    msg = ":: %s se ha unido al chat." % name
+    broadcast(bytes(msg, UTF_8))
     clients[client] = name
 
     while True:
+        # Escucha todo el tiempo a recibir mensajes.
+        # Si es la cadena salir, termina la conexión.
         msg = client.recv(BUFSIZ)
-        if msg != bytes("{quit}", "utf8"):
+        if msg != bytes("salir", UTF_8):
             broadcast(msg, name+": ")
         else:
-            client.send(bytes("{quit}", "utf8"))
+            client.send(bytes("salir", UTF_8))
             client.close()
             del clients[client]
-            broadcast(bytes("%s has left the chat." % name, "utf8"))
+            broadcast(bytes("%s has left the chat." % name, UTF_8))
             break
 
 
-def broadcast(msg, prefix=""):  # prefix is for name identification.
-    """Broadcasts a message to all the clients."""
+def broadcast(msg, prefix=""):  # el prefijo es para identificación por nombre
+    # Envía un mensaje a todos los clientes.
 
+    # Itera clientes, que es un arreglo de sockets y les envía el mensaje a todos.
     for sock in clients:
-        sock.send(bytes(prefix, "utf8")+msg)
+        sock.send(bytes(prefix, UTF_8)+msg)
 
         
 clients = {}
 addresses = {}
 
+# Levantamos el servidor en 127.0.0.1, que es localhost.
+# el puerto es el 8080, que fue elegido aleatoriamente, siempre que sea un puerto disponible.
+
 HOST = '127.0.0.1'
-PORT = 8080
+PORT = 8081
 BUFSIZ = 1024
 ADDR = (HOST, PORT)
 
 SERVER = socket(AF_INET, SOCK_STREAM)
 SERVER.bind(ADDR)
+CLIENTS_LIMIT = 10
 
 if __name__ == "__main__":
-    SERVER.listen(5)
-    print("Waiting for connection...")
+
+    SERVER.listen(CLIENTS_LIMIT)
+    print("Esperando nuevas conecciones...")
     ACCEPT_THREAD = Thread(target=accept_incoming_connections)
     ACCEPT_THREAD.start()
     ACCEPT_THREAD.join()
